7d94294df570914059f9069dd42350c9b9e6e95a
sadanon2544 <138858661+sadanon2544@users.noreply.github.com>
2023-07-10 14:12:36 +0700

    Update routes_line.py

--- route/routes_line.py
+++ route/routes_line.py
@@ -5,6 +5,6 @@ from config.db import collection_line
 lineRouter = APIRouter()
 
 @lineRouter.post("/post", tags=["user"])
-async def post_users(user: User):
+async def post_users(user: line_user):
     collection_line.insert_one(dict(user))
     return {"status": "OK"}
