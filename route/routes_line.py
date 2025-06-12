24055f828229cd9e6f8f05ddcf878ff49e791d4e
Arwatchai Yaemsaeng <69622448+yaemsaeng@users.noreply.github.com>
2025-06-12 14:23:01 +0700

    Update routes_line.py

--- route/routes_line.py
+++ route/routes_line.py
@@ -1,10 +1,1 @@
-from fastapi import APIRouter
-from model.models import line_user
-from config.db import collection_line
 
-lineRouter = APIRouter()
-
-@lineRouter.post("/post", tags=["user"])
-async def post_users(user: line_user):
-    collection_line.insert_one(dict(user))
-    return {"status": "OK"}
