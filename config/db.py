d485ea52514299ce73cefcf47022a51a7742e338
Arwatchai Yaemsaeng <69622448+yaemsaeng@users.noreply.github.com>
2025-06-16 12:54:17 +0700

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-db_connection = MongoClient("mongodb+srv://boat:1234@cluster0.rmsa1et.mongodb.net/")
+db_connection = MongoClient("mongodb+srv://boat:123456789_Za@cluster0.rmsa1et.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = db_connection.myDB
 collection = db["chat"]
 
